57ef65dde38f7b85e26b5c475af40255f94af02a
Agathius <p.akos1148@gmail.com>
2023-08-09 13:00:31 +0200

    installed required packages, set up vite server proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // Authentication page layout wrapper, context 
-import './App.css'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import FormContextProvider from '../context/FormContext'
+import "./App.css"
+import Login from "./containers/Login"
+import Signup from "./containers/Signup"
+import FormContextProvider from "../context/FormContext"
 
 function App() {
   return (
     <FormContextProvider>
-      <div className='page-layout'>
+      <div className="page-layout">
         <Signup/>
         <Login/>
       </div>
